0f0eed613262645102eaa29d49163c87a4b3baeb
bagdenius <bagdensparrow@gmail.com>
2026-03-25 01:03:42 +0200

    feat(prisma): add Prisma schema and service for PostgreSQL integration
    - Created a new Prisma schema for managing payments with fields for amount, status, providerId, and metadata.
    - Introduced PaymentStatus enum to represent different payment states.
    - Implemented PrismaModule and PrismaService to handle database connections and lifecycle events.
    - Exported PrismaModule and PrismaService for use in other parts of the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { PrismaModule } from './infra/prisma'
 
 @Module({
-	imports: []
+	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule]
 })
 export class AppModule {}
